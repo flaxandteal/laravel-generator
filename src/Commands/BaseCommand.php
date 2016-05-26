9bd505fa3baba01eadada7c84a061fd6e39fb815
Jim Schlies <jim@schlies.com>
2016-05-25 17:37:20 -0700

    style issue ???

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -196,7 +196,6 @@ class BaseCommand extends Command
     /**
      * @param $fileName
      * @param string $prompt
-     * 
      * @return bool
      */
     protected function confirmOverwrite($fileName, $prompt = '')
